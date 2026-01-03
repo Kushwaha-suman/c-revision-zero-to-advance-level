e6ce27be6d979369744f1f138ae80e383aea0c81
Suman <hamariadhurikahani333@gmail.com>
2026-01-03 22:12:45 +0545

    solving 2d arrays and creating all folder name

---
+++ 7 2D Arrays/02_AddMatrices.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"enter rows and column:\n";
+    cin>>r>>c;
+    int a[r][c],b[r][c],sum[r][c];
+    cout<<"enter first matrix:\n";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+            
+        }
+    } cout<<"enter second matrix:\n";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>b[i][j];
+            
+        }
+    }
+ for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            sum[i][j] = a[i][j] + b[i][j];
+
+    cout << "Sum matrix:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            cout << sum[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
